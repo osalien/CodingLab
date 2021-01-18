93c9a1c1f62fcc58ecca3ab60e67a9460d263a58
zhaoyuan <me@osalien.com>
2021-01-18 14:10:06 +0800

    db

--- docs/db/mysql_coding_lab.sql
+++ docs/db/mysql_coding_lab.sql
@@ -14,9 +14,9 @@
  * limitations under the License.
 */
 
-create database if not exists spring_boot_plus character set utf8mb4 COLLATE utf8mb4_general_ci;
+create database if not exists coding_lab character set utf8mb4 COLLATE utf8mb4_general_ci;
 
-use spring_boot_plus;
+use coding_lab;
 
 create table if not exists foo_bar
 (
