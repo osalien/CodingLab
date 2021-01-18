b7cb01ed8d781bfd82ad68e6c39a3d660630cee3
zhaoyuan <me@osalien.com>
2021-01-18 13:56:36 +0800

    Coding实验室

---
+++ bootstrap/src/main/java/com/osalien/www/CodingLabApplication.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019-2029 geekidea(https://github.com/geekidea)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.osalien.www;
+
+import com.osalien.www.framework.util.PrintApplicationInfo;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * coding-lab 项目启动入口
+ *
+ * @author geekidea
+ * @since 2018-11-08
+ */
+@EnableAsync
+@EnableScheduling
+@EnableTransactionManagement
+@EnableConfigurationProperties
+@ServletComponentScan
+@MapperScan({"com.osalien.www.**.mapper"})
+@SpringBootApplication(scanBasePackages = {"com.osalien.www"})
+public class CodingLabApplication {
+
+    public static void main(String[] args) {
+        // 启动
+        ConfigurableApplicationContext context = SpringApplication.run(CodingLabApplication.class, args);
+        // 打印项目信息
+        PrintApplicationInfo.print(context);
+        // 打印项目提示
+        PrintApplicationInfo.printTip(context);
+    }
+
+}
---
+++ generator/src/main/java/com/osalien/www/generator/CodingLabGenerator.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright 2019-2029 osalien(https://github.com/osalien)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.osalien.www.generator;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.osalien.www.generator.properties.GeneratorProperties;
+import com.osalien.www.generator.config.GeneratorStrategy;
+import com.osalien.www.generator.constant.GeneratorConstant;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * spring-boot-plus代码生成器入口类
+ *
+ * @author osalien
+ * @date 2019-10-22
+ **/
+@Component
+public class CodingLabGenerator {
+
+    /**
+     * 生成代码
+     * @param args
+     */
+    public static void main(String[] args) {
+        GeneratorProperties generatorProperties = new GeneratorProperties();
+
+        // 设置基本信息
+        generatorProperties
+                .setMavenModuleName("work")
+                .setParentPackage("com.osalien.www.work")
+                .setModuleName("foobar")
+                .setAuthor("osalien")
+                .setFileOverride(true);
+
+        // 设置表信息
+        generatorProperties.addTable("foo_bar","id");
+        // 设置表前缀
+        // generatorProperties.setTablePrefix(Arrays.asList("tb_"));
+
+        // 数据源配置
+        generatorProperties.getDataSourceConfig()
+                .setDbType(DbType.MYSQL)
+                .setUsername("test")
+                .setPassword("test")
+                .setDriverName("com.mysql.jdbc.Driver")
+                .setUrl("jdbc:mysql://47.102.155.178:3306/coding_lab?useUnicode=true&characterEncoding=UTF-8&useSSL=false");
+
+        // 生成配置
+        generatorProperties.getGeneratorConfig()
+                .setGeneratorStrategy(GeneratorStrategy.SINGLE)
+                .setGeneratorEntity(true)
+                .setGeneratorController(true)
+                .setGeneratorService(true)
+                .setGeneratorServiceImpl(true)
+                .setGeneratorMapper(true)
+                .setGeneratorMapperXml(true)
+                .setGeneratorPageParam(true)
+                .setGeneratorQueryVo(true)
+                .setRequiresPermissions(false)
+                .setPageListOrder(true)
+                .setParamValidation(true)
+                .setSwaggerTags(true)
+                .setOperationLog(true);
+
+        // 全局配置
+        generatorProperties.getMybatisPlusGeneratorConfig().getGlobalConfig()
+                .setOpen(true)
+                .setSwagger2(true)
+                .setIdType(IdType.AUTO)
+                .setDateType(DateType.ONLY_DATE);
+
+        // 策略配置
+        generatorProperties.getMybatisPlusGeneratorConfig().getStrategyConfig()
+                .setNaming(NamingStrategy.underline_to_camel)
+                .setColumnNaming(NamingStrategy.underline_to_camel)
+                .setEntityLombokModel(true)
+                .setRestControllerStyle(true)
+                .setControllerMappingHyphenStyle(true)
+                .setVersionFieldName(GeneratorConstant.VERSION)
+                .setLogicDeleteFieldName(GeneratorConstant.DELETED);
+
+        // 生成代码
+        CodeGenerator codeGenerator = new CodeGenerator();
+        codeGenerator.generator(generatorProperties);
+
+    }
+
+
+}
